3c747b45924fe6a2fb18d74fb2ad1f2a0fd1a8fc
teo2468 <tomo161219@gmail.com>
2026-04-17 17:20:30 +0900

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
     return res.status(405).send('Method Not Allowed');
   }
 
-  const GAS_URL = 'https://script.google.com/macros/s/AKfycbwYyGXKQXlhusXNFYYHMcxtkJQJTHRlvT4hawOsGnZ7qKsTRMV6QpCgukL4EZJrgODVkw/exec';
+  const GAS_URL = 'https://script.google.com/macros/s/AKfycbwElDA_KxO0_IpXaZCzgZHcGKpUvf-ee802X66nP0v42_DLa94TTlFkI5NFpuixeZW9hw/exec';
 
   // 先に200を返す（LINEのタイムアウト対策）
   res.status(200).json({ status: 'ok' });
