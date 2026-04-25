a486a669580c9f0da526d8ddbce1fb6d8881409a
teo2468 <tomo161219@gmail.com>
2026-04-26 03:24:02 +0900

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -3,7 +3,7 @@ module.exports = async function handler(req, res) {
     return res.status(405).send('Method Not Allowed');
   }
 
-  const GAS_URL = 'https://script.google.com/macros/s/AKfycbyd1QsM4Zcw3AqqU0n62yRDLOxbnz5F3n5h-i3l3dM-BuJZbO35sUNLA_pZRSxx4Iv8YA/exec';
+  const GAS_URL = 'https://script.google.com/macros/s/AKfycbyREDlXblHd1ICrEnnrM_CsnQ61jNACJ8nXRaRrnCPfBsunsu3mgM79EOG_MQACgugCRw/exec';
 
   const bodyStr = JSON.stringify(req.body);
   console.log('[webhook] received. events:', req.body?.events?.length ?? 0);
